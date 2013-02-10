28abc44af6124a978acfe34d1460e8e545917ca5
Łukasz Patalas <luke.patalas@gmail.com>
2013-02-10 15:00:18 +0100

    Create app bootstrapper

---
+++ NhLogAnalyzer/ShellViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhLogAnalyzer
+{
+	public class ShellViewModel
+	{
+		public string TitleText { get; set; }
+
+		public ShellViewModel()
+		{
+			TitleText = "ShellView";
+		}
+	}
+}
