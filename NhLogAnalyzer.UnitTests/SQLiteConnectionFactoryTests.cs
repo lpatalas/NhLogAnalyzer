ed4aedc04bca3cdbbb29bc5e87b4950615bbcb7d
Łukasz Patalas <luke.patalas@gmail.com>
2013-02-10 17:56:32 +0100

    Add SQLiteConnectionFactory class

---
+++ NhLogAnalyzer.UnitTests/SQLiteConnectionFactoryTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NhLogAnalyzer.UnitTests
+{
+	public class SQLiteConnectionFactoryTests
+	{
+		public class CreateConnectionMethod
+		{
+			[Fact]
+			public void Should_create_specify_valid_connection_string()
+			{
+				// Arrange
+				var connectionFactory = new SQLiteConnectionFactory();
+
+				// Act
+				var connection = connectionFactory.CreateConnection(@"X:\files.txt");
+				var connectionString = connection.ConnectionString;
+				connection.Dispose();
+
+				// Assert
+				Assert.Equal(@"Data Source=X:\files.txt;Version=3", connectionString);
+			}
+		}
+	}
+}
---
+++ NhLogAnalyzer/SQLiteConnectionFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhLogAnalyzer
+{
+	public class SQLiteConnectionFactory : IConnectionFactory
+	{
+		public IDbConnection CreateConnection(string fileName)
+		{
+			var connectionString = string.Format("Data Source={0};Version=3", fileName);
+			return new SQLiteConnection(connectionString);
+		}
+	}
+}
