a1b4649f01e11319100109511a51551abf8bab37
Łukasz Patalas <luke.patalas@gmail.com>
2013-02-10 18:09:15 +0100

    Add ninject package

---
+++ NhLogAnalyzer/DependencyInjectionModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject.Modules;
+
+namespace NhLogAnalyzer
+{
+	public class DependencyInjectionModule : NinjectModule
+	{
+		public override void Load()
+		{
+			Bind<IConnectionFactory>().To<SQLiteConnectionFactory>();
+			Bind<IStatementLog>().To<StatementLog>();
+			Bind<IStatementReader>().To<StatementReader>();
+		}
+	}
+}
