a83776177ee937ee771be65abd264d0f43f45438
Łukasz Patalas <luke.patalas@gmail.com>
2013-02-12 21:57:43 +0100

    Add StackTraceParser implementation

---
+++ NhLogAnalyzer.UnitTests/Infrastructure/StackTraceParserTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NhLogAnalyzer.Infrastructure;
+using Xunit;
+using Xunit.Extensions;
+
+namespace NhLogAnalyzer.UnitTests.Infrastructure
+{
+	public class StackTraceParserTests
+	{
+		public class ParseMethod
+		{
+			private readonly StackTraceParser stackTraceParser = new StackTraceParser();
+
+			[Theory]
+			[InlineData(null)]
+			[InlineData("")]
+			public void Should_return_empty_list_when_stackTrace_is_null_or_empty(
+				string input)
+			{
+				// Arrange
+
+				// Act
+				var output = stackTraceParser.Parse(input);
+
+				// Assert
+				Assert.NotNull(output);
+				Assert.Equal(0, output.Count);
+			}
+
+			[Fact]
+			public void Should_treat_each_line_as_single_stack_frame()
+			{
+				// Arrange
+				var input
+					= "ABC ABC:1:1\r\n"
+					+ "DEF DEF:1:1\n"
+					+ "GHI GHI:1:1";
+
+				// Act
+				var output = stackTraceParser.Parse(input);
+
+				// Assert
+				Assert.Equal(3, output.Count);
+			}
+
+			[Fact]
+			public void Should_interpret_characters_before_first_space_as_method_name()
+			{
+				// Arrange
+				var input = "method file:1:2";
+
+				// Act
+				var output = stackTraceParser.Parse(input);
+
+				// Assert
+				Assert.Equal("method", output[0].Method);
+			}
+
+			[Fact]
+			public void Should_interpret_characters_between_first_space_and_first_following_colon_as_file_name()
+			{
+				// Arrange
+				var input = "method file:1:2";
+
+				// Act
+				var output = stackTraceParser.Parse(input);
+
+				// Assert
+				Assert.Equal("file", output[0].FileName);
+			}
+
+			[Fact]
+			public void Should_set_file_name_to_empty_string_when_line_does_not_consist_of_two_parts_separated_by_space()
+			{
+				// Arrange
+				var input = "method";
+
+				// Act
+				var output = stackTraceParser.Parse(input);
+
+				// Assert
+				Assert.Equal("method", output[0].Method);
+			}
+
+			[Fact]
+			public void Should_parse_line_and_column_number_from_last_two_digits_separated_by_colons()
+			{
+				// Arrange
+				var input = "method file:11:22";
+
+				// Act
+				var output = stackTraceParser.Parse(input);
+
+				// Assert
+				Assert.Equal(11, output[0].Line);
+				Assert.Equal(22, output[0].Column);
+			}
+		}
+	}
+}
---
+++ NhLogAnalyzer/Infrastructure/StackTraceParser.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhLogAnalyzer.Infrastructure
+{
+	public class StackTraceParser : IStackTraceParser
+	{
+		public IList<StackFrame> Parse(string stackTrace)
+		{
+			if (string.IsNullOrEmpty(stackTrace))
+				return new List<StackFrame>();
+
+			var stackFrames = new List<StackFrame>();
+			var reader = new StringReader(stackTrace);
+			var line = reader.ReadLine();
+			
+			while (line != null)
+			{
+				var methodName = ParseMethodName(line);
+				var fileInfo = ParseFileName(line);
+
+				stackFrames.Add(new StackFrame(
+					methodName,
+					fileInfo.FileName,
+					fileInfo.Offset.Line,
+					fileInfo.Offset.Column));
+
+				line = reader.ReadLine();
+			}
+
+			return stackFrames;
+		}
+
+		private string ParseMethodName(string line)
+		{
+			var spaceIndex = line.IndexOf(' ');
+			if (spaceIndex > 0)
+				return line.Substring(0, spaceIndex);
+
+			return line;
+		}
+
+		private FileLocationInfo ParseFileName(string line)
+		{
+			var result = new FileLocationInfo { FileName = string.Empty };
+
+			var startIndex = line.IndexOf(' ');
+			if (startIndex > 0)
+			{
+				var locationIndex = line.IndexOf(':', startIndex);
+				if (locationIndex > 0)
+				{
+					result.FileName = line.Substring(startIndex + 1, locationIndex - startIndex - 1);
+					result.Offset = ParseFileOffset(line.Substring(locationIndex + 1));
+				}
+				else
+				{
+					result.FileName = line.Substring(startIndex + 1);
+				}
+			}
+
+			return result;
+		}
+
+		private FileOffset ParseFileOffset(string lineAndColumn)
+		{
+			var offset = new FileOffset();
+			var parts = lineAndColumn.Split(':');
+			int.TryParse(parts[0], out offset.Line);
+			int.TryParse(parts[1], out offset.Column);
+			return offset;
+		}
+
+		private struct FileLocationInfo
+		{
+			public string FileName;
+			public FileOffset Offset;
+		}
+
+		private struct FileOffset
+		{
+			public int Line;
+			public int Column;
+		}
+	}
+}
