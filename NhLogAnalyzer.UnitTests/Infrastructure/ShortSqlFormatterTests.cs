e21f47d7a922fb00fc3073435919baf5fa8b6419
Łukasz Patalas <luke.patalas@gmail.com>
2013-02-12 08:11:09 +0100

    Implement ShortSqlFormatter

---
+++ NhLogAnalyzer.UnitTests/Infrastructure/ShortSqlFormatterTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NhLogAnalyzer.Infrastructure;
+using Xunit;
+
+namespace NhLogAnalyzer.UnitTests.Infrastructure
+{
+	public class ShortSqlFormatterTests
+	{
+		public class FormatMethod
+		{
+			private readonly ShortSqlFormatter formatter = new ShortSqlFormatter();
+
+			[Fact]
+			public void Should_remove_leading_and_trailing_whitespace_from_SqlText()
+			{
+				// Arrange
+				var input = " \t\r\nABC\t \r\n ";
+
+				// Act
+				var output = formatter.Format(input);
+
+				// Assert
+				Assert.Equal("ABC", output);
+			}
+
+			[Fact]
+			public void Should_replace_all_line_breaks_with_single_space()
+			{
+				// Arrange
+				var input = "A\r\nB\nC";
+
+				// Act
+				var output = formatter.Format(input);
+
+				// Assert
+				Assert.Equal("A B C", output);
+			}
+
+			[Fact]
+			public void Should_replace_multiple_consecutive_white_space_characters_with_single_space()
+			{
+				// Arrange
+				var input = "A\n \tB\t\t \tC\r\n\r\nD";
+
+				// Act
+				var output = formatter.Format(input);
+
+				// Assert
+				Assert.Equal("A B C D", output);
+			}
+		}
+	}
+}
---
+++ NhLogAnalyzer/Infrastructure/ShortSqlFormatter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NhLogAnalyzer.Infrastructure
+{
+	public class ShortSqlFormatter : ISqlFormatter
+	{
+		private static readonly Regex whitespaceRegex = new Regex(@"[\r\n\t ]+");
+
+		public string Format(string input)
+		{
+			var trimmedInput = input.Trim();
+			return whitespaceRegex.Replace(trimmedInput, " ");
+		}
+	}
+}
