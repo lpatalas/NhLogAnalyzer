62844a8395ca38d267f07b1a8d838885c674fbbb
Łukasz Patalas <luke.patalas@gmail.com>
2013-02-12 08:14:33 +0100

    Implement dummy FullSqlFormatter

---
+++ NhLogAnalyzer.UnitTests/Infrastructure/FullSqlFormatterTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NhLogAnalyzer.Infrastructure;
+using Xunit;
+
+namespace NhLogAnalyzer.UnitTests.Infrastructure
+{
+	public class FullSqlFormatterTests
+	{
+		public class FormatMethod
+		{
+			[Fact]
+			public void Should_return_input_string()
+			{
+				// Arrange
+				var input = "SQL";
+				var formatter = new FullSqlFormatter();
+
+				// Act
+				var output = formatter.Format(input);
+
+				// Assert
+				Assert.Equal(input, output);
+			}
+		}
+	}
+}
---
+++ NhLogAnalyzer/Infrastructure/FullSqlFormatter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhLogAnalyzer.Infrastructure
+{
+	public class FullSqlFormatter : ISqlFormatter
+	{
+		public string Format(string input)
+		{
+			return input;
+		}
+	}
+}
