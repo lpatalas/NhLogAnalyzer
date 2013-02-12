4dfd5c2913b800107c99997a647b5b2426d64a42
Łukasz Patalas <luke.patalas@gmail.com>
2013-02-12 07:44:04 +0100

    Add default StatementMapper implementation

---
+++ NhLogAnalyzer.UnitTests/Infrastructure/StatementMapperTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NhLogAnalyzer.Infrastructure;
+using Xunit;
+
+namespace NhLogAnalyzer.UnitTests.Infrastructure
+{
+	public class StatementMapperTests
+	{
+		public class MapMethod
+		{
+			[Fact]
+			public void Should_copy_Id_and_Timestamp_properties_to_returned_row()
+			{
+				// Arrange
+				var input = new StatementRow(1, string.Empty, string.Empty, new DateTime(2013, 2, 12));
+				var statementMapper = new StatementMapper();
+
+				// Act
+				var output = statementMapper.Map(input);
+
+				// Assert
+				Assert.Equal(input.Id, output.Id);
+				Assert.Equal(input.Timestamp, output.Timestamp);
+			}
+		}
+	}
+}
---
+++ NhLogAnalyzer/Infrastructure/StatementMapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhLogAnalyzer.Infrastructure
+{
+	public class StatementMapper : IStatementMapper
+	{
+		public Statement Map(StatementRow row)
+		{
+			return new Statement(row.Id, row.SqlText, row.StackTrace, row.Timestamp);
+		}
+	}
+}
